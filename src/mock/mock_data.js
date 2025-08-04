5c680a01d437794e2666ca728589aec4afd6609b
xiongfei1121 <165855805+xiongfei1121@users.noreply.github.com>
2025-08-04 16:57:46 +0800

    Update mock_data.js

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -42,39 +42,11 @@ export const mockData = {
           "icon": "/sitelogo/spy.png"
         },
         {
-          "id": "site-1752857783656",
-          "name": "OpenAI",
-          "url": "https://chatgpt.com",
-          "description": "OpenAI,好用👌",
-          "icon": "/sitelogo/chatgpt.com.ico"
-        },
-        {
-          "id": "linux-do",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux技术社区，Peace and Love",
-          "icon": "/sitelogo/linux.do.ico"
-        },
-        {
-          "id": "site-1752649007053",
-          "name": "BeJson",
-          "url": "https://www.bejson.com/",
-          "description": "工具大全",
-          "icon": "/sitelogo/www.bejson.com.ico"
-        },
-        {
-          "id": "github",
-          "name": "GitHub",
-          "url": "https://github.com",
-          "description": "代码托管平台",
-          "icon": "/sitelogo/github.com.ico"
-        },
-        {
-          "id": "curlconverter",
-          "name": "curl converter",
-          "url": "https://curlconverter.com/",
-          "description": "curl命令转换工具",
-          "icon": "/sitelogo/curlconverter.com.ico"
+          "id": "weixin",
+          "name": "微信公众平台",
+          "url": "https://channels.weixin.qq.com/",
+          "description": "微信公众平台",
+          "icon": "/sitelogo/spy.png"
         }
       ]
     },
@@ -516,5 +488,5 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航"
+  "title": "德宝导航"
 }
