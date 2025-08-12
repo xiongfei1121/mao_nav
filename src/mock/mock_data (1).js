f786e7abd2fb91ade87982aff3a4eca8a86bed88
xiongfei1121 <165855805+xiongfei1121@users.noreply.github.com>
2025-08-12 15:52:29 +0800

    Add files via upload

---
+++ src/mock/mock_data (1).js
@@ -0,0 +1,622 @@
+export const mockData = {
+  "categories": [
+    {
+      "id": "my-favorites",
+      "name": "我的常用",
+      "icon": "💥",
+      "order": 0,
+      "sites": [
+        {
+          "id": "akrm",
+          "name": "安康融媒",
+          "url": "https://app.akrt.cn/",
+          "description": "安康融媒后台👌",
+          "icon": "/sitelogo/akrm.png"
+        },
+        {
+          "id": "ankang",
+          "name": "安康市人民政府网站",
+          "url": "https://www.ankang.gov.cn/",
+          "description": "安康市人民政府网站",
+          "icon": "/sitelogo/ankang.png"
+        },
+        {
+          "id": "akxw",
+          "name": "安康新闻网",
+          "url": "http://www.akxw.cn/",
+          "description": "安康新闻网",
+          "icon": "/sitelogo/akxw.png"
+        },
+        {
+          "id": "135editor",
+          "name": "135编辑器",
+          "url": "https://www.135editor.com/beautify_editor.html",
+          "description": "135编辑器",
+          "icon": "/sitelogo/135.png"
+        },
+        {
+          "id": "weixin_sph",
+          "name": "视频号助手",
+          "url": "https://channels.weixin.qq.com/",
+          "description": "视频号助手",
+          "icon": "/sitelogo/spy.png"
+        },
+        {
+          "id": "weixin",
+          "name": "微信公众平台",
+          "url": "https://channels.weixin.qq.com/",
+          "description": "微信公众平台",
+          "icon": "/sitelogo/wx.png"
+        },
+        {
+          "id": "site-1754305963012",
+          "name": "抖音创作者中心",
+          "url": "https://creator.douyin.com/creator-micro/home",
+          "description": "",
+          "icon": "/sitelogo/creator.douyin.com.ico"
+        },
+        {
+          "id": "site-1754558335097",
+          "name": "MCH融合工作平台",
+          "url": "http://mch.aktv.com.cn:19207",
+          "description": "",
+          "icon": "/sitelogo/mch.png"
+        }
+      ]
+    },
+    {
+      "id": "community",
+      "name": "央媒省媒",
+      "icon": "👥",
+      "order": 1,
+      "sites": [
+        {
+          "id": "xinhuanet",
+          "name": "新华网",
+          "url": "https://www.xinhuanet.com/",
+          "description": "新华网",
+          "icon": "/sitelogo/xinhuanet.png"
+        },
+        {
+          "id": "people",
+          "name": "人民网",
+          "url": "http://www.people.com.cn/",
+          "description": "人民网",
+          "icon": "/sitelogo/people.png"
+        },
+        {
+          "id": "cctv",
+          "name": "央视新闻",
+          "url": "https://news.cctv.com/",
+          "description": "央视新闻",
+          "icon": "/sitelogo/cctv.jpg"
+        },
+        {
+          "id": "sxdaily",
+          "name": "陕西日报",
+          "url": "https://esb.sxdaily.com.cn/",
+          "description": "陕西日报",
+          "icon": "/sitelogo/sxdaily.png"
+        },
+        {
+          "id": "site-1754308735206",
+          "name": "学习强国",
+          "url": "https://gonggao2.xuexi.cn/?logout=1",
+          "description": "供稿系统",
+          "icon": "/sitelogo/gonggao2.xuexi.cn.ico"
+        }
+      ]
+    },
+    {
+      "id": "office",
+      "name": "办公工具",
+      "icon": "💼",
+      "order": 2,
+      "sites": [
+        {
+          "id": "site-1754560084710",
+          "name": "秀米",
+          "url": "https://xiumi.us/#/",
+          "description": "",
+          "icon": "https://edt.xiumius.cn/images/b80cd8.xiumi_logo_40.png"
+        },
+        {
+          "id": "site-1754301864179",
+          "name": "政府网传视频",
+          "url": "https://vod.ankang.gov.cn/login.html",
+          "description": "政府网传安康新闻、金州播报视频",
+          "icon": "https://web.081213.xyz:8899/uploads/202506/01JYKCZXHE6PGEFCNS1YKT5DND.ico"
+        },
+        {
+          "id": "xunfei",
+          "name": "讯飞智作",
+          "url": "https://peiyin.xunfei.cn/",
+          "description": "讯飞智作",
+          "icon": "/sitelogo/xunfei.png"
+        },
+        {
+          "id": "xiezuocat",
+          "name": "秘塔写作猫",
+          "url": "https://xiezuocat.com/",
+          "description": "秘塔写作猫",
+          "icon": "/sitelogo/xiezuocat.ico"
+        },
+        {
+          "id": "feishu",
+          "name": "飞书",
+          "url": "https://www.feishu.cn",
+          "description": "企业协作平台",
+          "icon": "/sitelogo/www.feishu.cn.ico"
+        },
+        {
+          "id": "site-1754301958039",
+          "name": "腾讯文档",
+          "url": "https://docs.qq.com/",
+          "description": "腾讯文档",
+          "icon": "https://web.081213.xyz:8899/uploads/202507/01JZA6X2797DWWVE93MTYVZBNQ.png"
+        },
+        {
+          "id": "site-1754302023964",
+          "name": "德宝聚合网盒",
+          "url": "https://alist.081213.xyz:8899",
+          "description": "",
+          "icon": "https://web.081213.xyz:8899/uploads/202506/01JYNMR1MPXFK0ZYWHKP0B4R45.png"
+        },
+        {
+          "id": "site-1754308797429",
+          "name": "腾讯邮箱",
+          "url": "https://mail.qq.com/",
+          "description": "",
+          "icon": "/sitelogo/mail.qq.com.ico"
+        },
+        {
+          "id": "site-1754560308999",
+          "name": "飞鹰智能校对",
+          "url": "https://feiying.iflytek.com/#/main",
+          "description": ""
+        },
+        {
+          "id": "site-1754560696495",
+          "name": "Notion",
+          "url": "https://www.notion.so",
+          "description": "",
+          "icon": "/sitelogo/www.notion.so.ico"
+        },
+        {
+          "id": "site-1754872456342",
+          "name": "GET笔记",
+          "url": "https://www.biji.com/",
+          "description": "",
+          "icon": "/sitelogo/getbiji.ico"
+        },
+        {
+          "id": "site-1754881975391",
+          "name": "腾讯ima",
+          "url": "https://ima.qq.com/",
+          "description": "知识库",
+          "icon": "/sitelogo/ima.svg"
+        }
+      ]
+    },
+    {
+      "id": "ai-tools",
+      "name": "AI智能",
+      "icon": "🤖",
+      "order": 3,
+      "sites": [
+        {
+          "id": "site-1754309898005",
+          "name": "即梦AI",
+          "url": "https://jimeng.jianying.com/ai-tool/home",
+          "description": "",
+          "icon": "/sitelogo/jimeng.jianying.com.ico"
+        },
+        {
+          "id": "site-1754310988268",
+          "name": "可灵AI",
+          "url": "https://app.klingai.com/cn/",
+          "description": "",
+          "icon": "/sitelogo/app.klingai.com.ico"
+        },
+        {
+          "id": "site-1754309924525",
+          "name": "deepseek",
+          "url": "https://www.deepseek.com/",
+          "description": "",
+          "icon": "/sitelogo/www.deepseek.com.ico"
+        },
+        {
+          "id": "site-1754309955124",
+          "name": "Kimi",
+          "url": "https://www.kimi.com/",
+          "description": "",
+          "icon": "/sitelogo/www.kimi.com.ico"
+        },
+        {
+          "id": "site-1754310014109",
+          "name": "腾讯元宝",
+          "url": "https://yuanbao.tencent.com/",
+          "description": "",
+          "icon": "/sitelogo/yuanbao.tencent.com.ico"
+        },
+        {
+          "id": "site-1754310129620",
+          "name": "通义千问",
+          "url": "https://www.tongyi.com/",
+          "description": "",
+          "icon": "/sitelogo/tyqw.png"
+        },
+        {
+          "id": "site-1754310313276",
+          "name": "文心一言",
+          "url": "https://yiyan.baidu.com/",
+          "description": "",
+          "icon": "/sitelogo/yiyan.baidu.com.ico"
+        },
+        {
+          "id": "site-1754310335868",
+          "name": "豆包",
+          "url": "https://www.doubao.com/chat/",
+          "description": "",
+          "icon": "/sitelogo/www.doubao.com.ico"
+        },
+        {
+          "id": "site-1754310770484",
+          "name": "硅基流动",
+          "url": "https://cloud.siliconflow.cn/",
+          "description": "",
+          "icon": "/sitelogo/cloud.siliconflow.cn.ico"
+        },
+        {
+          "id": "site-1754312803302",
+          "name": "AI工具集",
+          "url": "https://ai-bot.cn/",
+          "description": "",
+          "icon": "/sitelogo/ai-bot.cn.ico"
+        },
+        {
+          "id": "chatgpt",
+          "name": "ChatGPT",
+          "url": "https://chat.openai.com",
+          "description": "OpenAI对话AI助手",
+          "icon": "/sitelogo/chat.openai.com.ico"
+        },
+        {
+          "id": "site-1754356244393",
+          "name": "AI人声去除器",
+          "url": "https://aivocalremover.com/",
+          "icon": "/sitelogo/aivocalremover.png"
+        },
+        {
+          "id": "site-1754449164427",
+          "name": "智谱AI",
+          "url": "https://open.bigmodel.cn/console/overview",
+          "description": "",
+          "icon": "/sitelogo/open.bigmodel.cn.ico"
+        },
+        {
+          "id": "site-1754788816839",
+          "name": "扣子",
+          "url": "https://www.coze.cn",
+          "description": "",
+          "icon": "/sitelogo/coze.png"
+        }
+      ]
+    },
+    {
+      "id": "design",
+      "name": "设计工具",
+      "icon": "🎨",
+      "order": 4,
+      "sites": [
+        {
+          "id": "site-1754312698876",
+          "name": "凡科快图",
+          "url": "https://kt.fkw.com/sheji.html#/m/index",
+          "description": "",
+          "icon": "/sitelogo/kt.fkw.com.ico"
+        },
+        {
+          "id": "site-1754312751723",
+          "name": "即时设计",
+          "url": "https://js.design/",
+          "description": "",
+          "icon": "/sitelogo/js.design.ico"
+        },
+        {
+          "id": "figma",
+          "name": "Figma",
+          "url": "https://figma.com",
+          "description": "UI设计工具",
+          "icon": "/sitelogo/figma.com.ico"
+        },
+        {
+          "id": "sketch",
+          "name": "Sketch",
+          "url": "https://www.sketch.com",
+          "description": "界面设计工具",
+          "icon": "/sitelogo/www.sketch.com.ico"
+        },
+        {
+          "id": "canva",
+          "name": "Canva",
+          "url": "https://www.canva.com",
+          "description": "在线设计平台",
+          "icon": "/sitelogo/www.canva.com.ico"
+        },
+        {
+          "id": "adobe-xd",
+          "name": "Adobe XD",
+          "url": "https://www.adobe.com/products/xd.html",
+          "description": "用户体验设计工具",
+          "icon": "/sitelogo/www.adobe.com.ico"
+        },
+        {
+          "id": "site-1754560210776",
+          "name": "创客贴",
+          "url": "https://www.chuangkit.com/",
+          "description": "",
+          "icon": "https://www.chuangkit.com/distsaas/img/logo.7f1eaf73.svg"
+        },
+        {
+          "id": "site-1754613081421",
+          "name": "阿里巴巴矢量图标库",
+          "url": "https://www.iconfont.cn/",
+          "description": "",
+          "icon": "/sitelogo/www.iconfont.cn.png"
+        },
+        {
+          "id": "site-1754965307703",
+          "name": "字节跳动图标库",
+          "url": "https://iconpark.oceanengine.com/",
+          "description": "图标库、插画库",
+          "icon": "/sitelogo/iconpark.svg"
+        }
+      ]
+    },
+    {
+      "id": "cloud",
+      "name": "云服务",
+      "icon": "☁️",
+      "order": 5,
+      "sites": [
+        {
+          "id": "site-1754311080075",
+          "name": "宝塔面板",
+          "url": "https://117.36.154.21:29436",
+          "description": "",
+          "icon": "/sitelogo/www.bt.cn.ico"
+        },
+        {
+          "id": "cloudflare",
+          "name": "Cloudflare",
+          "url": "https://www.cloudflare.com",
+          "description": "全球CDN和网络安全服务",
+          "icon": "/sitelogo/www.cloudflare.com.ico"
+        },
+        {
+          "id": "vercel",
+          "name": "Vercel",
+          "url": "https://vercel.com",
+          "description": "前端部署平台",
+          "icon": "/sitelogo/vercel.com.ico"
+        },
+        {
+          "id": "aws",
+          "name": "AWS",
+          "url": "https://aws.amazon.com",
+          "description": "亚马逊云服务",
+          "icon": "/sitelogo/aws.amazon.com.ico"
+        },
+        {
+          "id": "aliyun",
+          "name": "阿里云",
+          "url": "https://www.aliyun.com",
+          "description": "阿里巴巴云计算",
+          "icon": "/sitelogo/www.aliyun.com.ico"
+        },
+        {
+          "id": "tencent-cloud",
+          "name": "腾讯云",
+          "url": "https://cloud.tencent.com",
+          "description": "腾讯云计算服务",
+          "icon": "/sitelogo/cloud.tencent.com.ico"
+        },
+        {
+          "id": "huawei-cloud",
+          "name": "华为云",
+          "url": "https://www.huaweicloud.com",
+          "description": "华为云计算服务",
+          "icon": "/sitelogo/www.huaweicloud.com.ico"
+        },
+        {
+          "id": "site-1752644060499",
+          "name": "怕死云",
+          "url": "https://www.pasyun.com/",
+          "description": "ipv6机器",
+          "icon": "/sitelogo/www.pasyun.com.ico"
+        },
+        {
+          "id": "site-1754962971771",
+          "name": "爪云",
+          "url": "https://eu-central-1.run.claw.cloud/",
+          "description": "",
+          "icon": "/sitelogo/eu-central-1.run.claw.cloud.png"
+        }
+      ]
+    },
+    {
+      "id": "learning",
+      "name": "学习资源",
+      "icon": "📚",
+      "order": 7,
+      "sites": [
+        {
+          "id": "site-1754306059372",
+          "name": "陕西省专业技术人员继续教育网",
+          "url": "http://jxjy.xidian.edu.cn/",
+          "description": "继续教育",
+          "icon": "/sitelogo/jxjy.xidian.edu.cn.ico"
+        },
+        {
+          "id": "site-1754309006111",
+          "name": "智慧广电学院",
+          "url": "https://training.nrta.gov.cn/",
+          "description": "",
+          "icon": "/sitelogo/zhgd.png"
+        },
+        {
+          "id": "site-1754356465360",
+          "name": "国家中小学智慧教育平台",
+          "url": "https://basic.smartedu.cn/",
+          "description": "",
+          "icon": "/sitelogo/basic.smartedu.cn.ico"
+        },
+        {
+          "id": "site-1754356876111",
+          "name": "Phet模拟实验官网",
+          "url": "https://phet.colorado.edu/zh_CN/",
+          "description": "免费的在线物理、化学、生物、地理及数学仿真程序",
+          "icon": "/sitelogo/phet.colorado.edu.ico"
+        },
+        {
+          "id": "site-1754377944608",
+          "name": "全球经典儿童睡前故事",
+          "url": "https://beddystories.com/zh",
+          "description": "",
+          "icon": "/sitelogo/beddystories.com.ico"
+        }
+      ]
+    },
+    {
+      "id": "tools",
+      "name": "在线工具",
+      "icon": "⚙️",
+      "order": 8,
+      "sites": [
+        {
+          "id": "json-formatter",
+          "name": "JSON Formatter",
+          "url": "https://jsonformatter.org",
+          "description": "JSON格式化工具",
+          "icon": "/sitelogo/jsonformatter.org.ico"
+        },
+        {
+          "id": "regex101",
+          "name": "Regex101",
+          "url": "https://regex101.com",
+          "description": "正则表达式测试",
+          "icon": "/sitelogo/regex101.com.ico"
+        },
+        {
+          "id": "caniuse",
+          "name": "Can I Use",
+          "url": "https://caniuse.com",
+          "description": "浏览器兼容性查询",
+          "icon": "/sitelogo/caniuse.com.ico"
+        },
+        {
+          "id": "tinypng",
+          "name": "TinyPNG",
+          "url": "https://tinypng.com",
+          "description": "图片压缩工具",
+          "icon": "/sitelogo/tinypng.com.ico"
+        },
+        {
+          "id": "site-1754309464142",
+          "name": "帮小忙",
+          "url": "https://tool.browser.qq.com/",
+          "description": "",
+          "icon": "/sitelogo/tool.browser.qq.com.ico"
+        },
+        {
+          "id": "site-1754309570439",
+          "name": "便民查询网",
+          "url": "https://www.bmcx.com/",
+          "description": "",
+          "icon": "/sitelogo/www.bmcx.com.ico"
+        },
+        {
+          "id": "site-1754356370080",
+          "name": "在线抠图",
+          "url": "https://remove.photos/zh-cn/",
+          "description": "",
+          "icon": "/sitelogo/remove.photos.ico"
+        }
+      ]
+    },
+    {
+      "id": "entertainment",
+      "name": "娱乐休闲",
+      "icon": "🎮",
+      "order": 9,
+      "sites": [
+        {
+          "id": "bilibili",
+          "name": "哔哩哔哩",
+          "url": "https://www.bilibili.com",
+          "description": "弹幕视频网站",
+          "icon": "/sitelogo/www.bilibili.com.ico"
+        },
+        {
+          "id": "youtube",
+          "name": "YouTube",
+          "url": "https://www.youtube.com",
+          "description": "视频分享平台",
+          "icon": "/sitelogo/www.youtube.com.ico"
+        },
+        {
+          "id": "douban",
+          "name": "豆瓣",
+          "url": "https://www.douban.com",
+          "description": "文艺生活社区",
+          "icon": "/sitelogo/www.douban.com.ico"
+        },
+        {
+          "id": "zhihu",
+          "name": "知乎",
+          "url": "https://www.zhihu.com",
+          "description": "知识问答社区",
+          "icon": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='black'%3E%3Cpath d='M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.11 0 0 .67-.21 2.2.82A7.65 7.65 0 018 4.58c.68.003 1.36.092 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.91.08 2.11.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0016 8c0-4.42-3.58-8-8-8z'/%3E%3C/svg%3E"
+        },
+        {
+          "id": "site-1754306219211",
+          "name": "德宝的影视站",
+          "url": "https://v.ztt.qzz.io",
+          "description": "影视站",
+          "icon": "/sitelogo/dbys.png"
+        },
+        {
+          "id": "site-1754313445291",
+          "name": "观影",
+          "url": "https://www.gying.org/",
+          "description": "",
+          "icon": "/sitelogo/gying.png"
+        },
+        {
+          "id": "site-1754313489139",
+          "name": "6V电影",
+          "url": "https://www.6vdy.org/",
+          "description": "",
+          "icon": "/sitelogo/www.6vdy.org.ico"
+        }
+      ]
+    },
+    {
+      "id": "category-1754359657025",
+      "icon": "🎵",
+      "name": "音乐",
+      "order": 0,
+      "sites": [
+        {
+          "id": "site-1754359689783",
+          "name": "无版权音乐库",
+          "url": "https://www.freesound.cn/",
+          "description": "",
+          "icon": "/sitelogo/www.freesound.cn.ico"
+        }
+      ]
+    }
+  ],
+  "title": "德宝导航站"
+}
